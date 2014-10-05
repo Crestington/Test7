3666faf15db99d2065c70e31c8ca1fbbdda99824
Crestington <Zelus2009@hotmail.com>
2014-10-04 20:39:50 -0700

    initial commit

---
+++ share/qt/make_windows_icon.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# create multiresolution windows icon
+ICON_DST=../../src/qt/res/icons/Colossuscoin.ico
+
+convert ../../src/qt/res/icons/logo_sharp_16.png ../../src/qt/res/icons/logo_sharp_24.png ../../src/qt/res/icons/logo_sharp_32.png ../../src/qt/res/icons/logo_sharp_48.png ../../src/qt/res/icons/logo_sharp_64.png ../../src/qt/res/icons/logo_sharp_128.png ../../src/qt/res/icons/logo_sharp_256.png ${ICON_DST}
